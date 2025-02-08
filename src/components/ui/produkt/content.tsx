0a7981560c0090b43d9904331fb06ae405b510ff
anlander <anlanderp@gmail.com>
2025-02-08 20:57:28 +0100

    width

--- src/components/ui/produkt/content.tsx
+++ src/components/ui/produkt/content.tsx
@@ -5,7 +5,7 @@ import { render } from "storyblok-rich-text-react-renderer";
 export const Content = ({ content }: any) => {
   return (
     <div className="container">
-      <div className="flex flex-col gap-5 w-full lg:max-w-[60%] mb-20 -mt-24 lg:-mt-14">
+      <div className="flex flex-col gap-5 w-full lg:max-w-[80%] mb-20 -mt-24 lg:-mt-14">
         {render(content)}
       </div>
     </div>
