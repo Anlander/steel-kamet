1517adb4153046a0a1af81e0c1086a80881cc55c
anlander <anlanderp@gmail.com>
2025-02-08 22:35:26 +0100

    logo

--- src/components/ui/Header/nav.tsx
+++ src/components/ui/Header/nav.tsx
@@ -13,7 +13,7 @@ export const Nav = ({ settings }: SettingsProps) => {
   return (
     <nav className="fixed w-full z-50 p-4 flex gap-5 lg:gap-0 items-center  flex-col lg:flex-row lg:justify-end bg-white">
       <Link href="/" className="lg:absolute left-4 top-2">
-        <Image src={settings.logo.filename} width={50} height={50} alt="logo" />
+        <Image src={settings.logo.filename} width={180} height={50} alt="logo" />
       </Link>
       <div className="flex justify-center uppercase items-center gap-5 lg:pr-10 font-bold">
         <Link href="/#tjänster">Tjänster</Link>
