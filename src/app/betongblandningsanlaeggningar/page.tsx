4ebba999c8076339383e32fb00a88ef2526bbc6c
anlander <anlanderp@gmail.com>
2025-02-08 17:57:55 +0100

    betong

--- src/app/betongblandningsanlaeggningar/page.tsx
+++ src/app/betongblandningsanlaeggningar/page.tsx
@@ -33,7 +33,7 @@ const Page = async () => {
       />
       <div className="grid grid-cols-1 gap-14">
         <div className="flex flex-col gap-5">
-          <h3 className="font-bold">WET MIX</h3>
+          <h3 className="font-bold">VÅTBETONG</h3>
           <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
             {data
               .filter((item: any) => item.content.category.includes("wetmix"))
@@ -43,7 +43,7 @@ const Page = async () => {
           </div>
         </div>
         <div className="flex flex-col gap-5">
-          <h3 className="font-bold">DRY MIX</h3>
+          <h3 className="font-bold">TORRBETONG</h3>
           <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
             {data
               .filter((item: any) => item.content.category.includes("drymix"))
