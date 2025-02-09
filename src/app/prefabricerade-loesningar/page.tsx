1d7b53a6130bf0bf46f4a0dd094c3acd8b543bdd
anlander <anlanderp@gmail.com>
2025-02-09 20:56:07 +0100

    fix

--- src/app/prefabricerade-loesningar/page.tsx
+++ src/app/prefabricerade-loesningar/page.tsx
@@ -37,18 +37,6 @@ const Page = async () => {
               ))}
           </div>
         </div>
-        <div className="flex flex-col gap-5" id="TILLBEHÖR FAMILJ">
-          <h3 className="font-bold">Tillbehör familjen</h3>
-          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
-            {data
-              .filter((item: any) =>
-                item.content.category.includes("tillbehor")
-              )
-              .map((item: any) => (
-                <Card key={item.uuid} item={item} />
-              ))}
-          </div>
-        </div>
       </div>
     </div>
   );
