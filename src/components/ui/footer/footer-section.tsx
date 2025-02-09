7d037b3d32a451c9f089dab8c9cb42d466b5d5b1
anlander <anlanderp@gmail.com>
2025-02-09 02:17:09 +0100

    footer

--- src/components/ui/footer/footer-section.tsx
+++ src/components/ui/footer/footer-section.tsx
@@ -25,12 +25,13 @@ const FooterSection = ({ settings }: FooterProps) => {
         </div>
 
         <div className="text-center mb-5 lg:mb-0 flex flex-col gap-2">
-          <span> © 2025 Steel Kamet. All rights reserved. </span>
-          <h3>Kontakt</h3>
+          
+          <h3>Direkt kontakt</h3>
           <Link href="tel:+46 703 762 380">+46 703 762 380</Link>
           <Link href="mailto:rosemarie.beckman@steelkamet.se">
             sales@steelkamet.se
           </Link>
+          <span className="text-sm"> © 2025 Steel Kamet. All rights reserved. </span>
         </div>
       </div>
     </div>
